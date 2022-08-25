0ab3b144275e5ab4b31f9e79d9611850b9dc2b10
BaoTruong <truonggiabao3@gmail.com>
2022-08-25 17:36:56 +0700

    refactor code, add a loop in supported_region

--- cdk_pipeline/pipeline_stacks.py
+++ cdk_pipeline/pipeline_stacks.py
@@ -17,39 +17,31 @@ import os
 class PipelineStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs):
         super().__init__(scope, id, **kwargs)
+        self.supported_regions = ["us-east-1", "us-east-2"]
 
         source_artifact = codepipeline.Artifact()
         cloud_assembly_artifact = codepipeline.Artifact()
 
-        pipeline = pipelines.CodePipeline(self, 'Pipeline',
+        self.pipeline = pipelines.CodePipeline(self, 'Pipeline',
 
 
-                                          pipeline_name='WebinarPipeline',
+                                               pipeline_name='WebinarPipeline',
 
-                                          synth=pipelines.ShellStep("Synth",
-                                                                    input=pipelines.CodePipelineSource.git_hub(
-                                                                        "Bao-Truong/cdkpipeline", "master", authentication=SecretValue.secrets_manager('github-token'),
-                                                                        trigger=cpactions.GitHubTrigger.POLL),
-                                                                    commands=[
-                                                                        "npm install -g aws-cdk", "python -m pip install -r requirements.txt", "cdk synth"]
-                                                                    )
-                                          )
+                                               synth=pipelines.ShellStep("Synth",
+                                                                         input=pipelines.CodePipelineSource.git_hub(
+                                                                             "Bao-Truong/cdkpipeline", "master", authentication=SecretValue.secrets_manager('github-token'),
+                                                                             trigger=cpactions.GitHubTrigger.POLL),
+                                                                         commands=[
+                                                                             "npm install -g aws-cdk", "python -m pip install -r requirements.txt", "cdk synth"]
+                                                                         )
+                                               )
+        self.distribute_to_region()
 
-        wave = pipeline.add_wave("wave")
-
-        useast1_stage = wave.add_stage(WebserverStage(self, "webserStage-useast-1",
-                                                      env=cdk.Environment(account=os.environ.get(
-                                                          "CDK_DEFAULT_ACCOUNT"), region="us-east-1")
-                                                      ))
-        useast2_stage = wave.add_stage(WebserverStage(self, "webserStage-useast-2",
-                                                      env=cdk.Environment(account=os.environ.get(
-                                                          "CDK_DEFAULT_ACCOUNT"), region="us-east-2")
-                                                      ))
-        # testing_stage = pipeline.add_stage(WebserverStage(self, "webserStage",
-        #                                                   env=cdk.Environment(account=os.environ.get(
-        #                                                       "CDK_DEFAULT_ACCOUNT"), region=os.environ.get("CDK_DEFAULT_ACCOUNT"))
-        #                                                   ))
-        # testing_stage.add_post(ManualApprovalStep('approval'))
-
-        useast1_stage.add_pre(pipelines.ManualApprovalStep('approval'))
-        useast2_stage.add_pre(pipelines.ManualApprovalStep('approval'))
+    def distribute_to_region(self):
+        wave = self.pipeline.add_wave("wave")
+        for region in self.supported_regions:
+            stage = wave.add_stage(WebserverStage(self, f"webserStage-{region}",
+                                                        env=cdk.Environment(account=os.environ.get(
+                                                            "CDK_DEFAULT_ACCOUNT"), region=region)
+                                                  ))
+            stage.add_pre(pipelines.ManualApprovalStep('approval'))
